db6b5a35c965b40cea50058ca51f812e8b93cfb5
Vyacheslav Koscheev <vok1980@gmail.com>
2015-09-04 11:08:20 +0600

    impl ver/platf struct

--- ftpBCM.py
+++ ftpBCM.py
@@ -2,12 +2,48 @@
 # -*- coding: utf-8 -*-
 
 import argparse
+from ftplib import FTP
+import ftplib
 
 
 class FtpBCM:
-	def __init__(self, command, server, path, version, platform):
-		print command, server, path, version, platform	
+	def __init__(self, command, server, user, passwd, path, version, platform):
+		print command, server, user, passwd, path, version, platform	
+		ftp = FTP(server)
+		ftp.login(user, passwd)
+		
+		self.__mkd_cd(ftp, 'bcm')
+		self.__mkd_cd(ftp, version)
+		self.__mkd_cd(ftp, platform)
 
+		ftp.retrlines('LIST')
+
+		if command == 'push':
+			self.__push(path)
+
+		if command == 'pull':
+			self.__pull(path)
+		
+		ftp.quit()
+
+
+	def __mkd_cd(self, ftp, dirname):
+		try:
+                        ftp.mkd(dirname)
+                except ftplib.error_perm as e:
+                        print(e)
+
+                ftp.cwd(dirname)
+
+
+	def __push(self, path):
+		print 'ready to push ', path
+
+
+	def __pull(self, path):
+		print 'ready to pull', path
+
+		
 
 
 def main():
@@ -17,14 +53,16 @@ def main():
 	parser.add_argument('path', help='Path to be stored on server')
 	parser.add_argument('version', help='Version of binaries')
 	parser.add_argument('platform', help='Target platform name')
+	parser.add_argument('--user', default='bcm', help='ftp username')
+	parser.add_argument('--passwd', default='123', help='ftp userpass')
 
 	args = parser.parse_args()
 
-	if ((args.command != 'push')  and (args.command != 'pull')):
+	if ((args.command != 'push') and (args.command != 'pull')):
 		print 'Unexpected command: ',  args.command
 		raise Exception('Unexpected command')
 
-	bcm = FtpBCM(args.command, args.server, args.path, args.version, args.platform);
+	bcm = FtpBCM(args.command, args.server, args.user, args.passwd, args.path, args.version, args.platform);
 
 
 if __name__ == "__main__":
