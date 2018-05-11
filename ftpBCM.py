641aaff4b3b5568f401d169e9dcc90a12f12438a
Vyacheslav Koscheev <v.koscheev@2gis.ru>
2018-05-11 19:02:48 +0700

    check exceptions on ftp login

--- ftpBCM.py
+++ ftpBCM.py
@@ -30,8 +30,12 @@ class FtpBCM:
 
 
 	def __login(self, version, platform):
-		self.ftp = ftplib.FTP(self.server)
-		self.ftp.login(self.user, self.passwd)
+		try:
+			self.ftp = ftplib.FTP(self.server)
+			self.ftp.login(self.user, self.passwd)
+		except Exception as e:
+			print "Failed fo login", self.server
+			raise e
 		self.__mkd_cd('bcm')
 		self.__mkd_cd(self.project)
 		self.__mkd_cd(version)
